42b16769a995ed231b0764740da7d0b29c9930d1
9898s <sueh9898@icloud.com>
2023-10-04 23:18:49 +0900

    test: 채용등록 수정 테스트 코드 작성

--- src/test/java/com/example/wanted/recruitment/service/RecruitmentServiceTest.java
+++ src/test/java/com/example/wanted/recruitment/service/RecruitmentServiceTest.java
@@ -4,9 +4,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import com.example.wanted.company.repository.CompanyRepository;
 import com.example.wanted.global.exception.CompanyException;
+import com.example.wanted.global.exception.RecruitmentException;
 import com.example.wanted.global.exception.type.ErrorCode;
 import com.example.wanted.recruitment.entity.Recruitment;
 import com.example.wanted.recruitment.model.AddRecruitment;
+import com.example.wanted.recruitment.model.EditRecruitment;
 import com.example.wanted.recruitment.repository.RecruitmentRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -61,5 +63,45 @@ class RecruitmentServiceTest {
 
     // then
     assertEquals(ErrorCode.INVALID_COMPANY_ID, companyException.getErrorCode());
+  }
+
+  @Test
+  void 채용공고_수정_테스트_성공() {
+    // given
+    Recruitment recruitment = recruitmentRepository.save(
+        Recruitment.builder()
+            .position("백엔드 주니어 개발자")
+            .build()
+    );
+
+    EditRecruitment.Request request = new EditRecruitment.Request();
+    request.setPosition("프론트엔드 주니어 개발자");
+
+    // when
+    String position = recruitmentService.editRecruitment(recruitment.getId(), request)
+        .getPosition();
+
+    // then
+    assertEquals(position, request.getPosition());
+  }
+
+  @Test
+  void 채용공고_수정_테스트_실패() {
+    // given
+    Recruitment recruitment = recruitmentRepository.save(
+        Recruitment.builder()
+            .position("백엔드 주니어 개발자")
+            .build()
+    );
+
+    EditRecruitment.Request request = new EditRecruitment.Request();
+    request.setPosition("프론트엔드 주니어 개발자");
+
+    // when
+    RecruitmentException recruitmentException = Assertions.assertThrows(RecruitmentException.class,
+        () -> recruitmentService.editRecruitment(2L, request));
+
+    // then
+    assertEquals(ErrorCode.INVALID_RECRUITMENT_ID, recruitmentException.getErrorCode());
   }
 }
